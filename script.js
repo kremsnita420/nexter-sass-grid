ae9de6260c36c33972376820b49daf805a8b1019
kremsnita420 <duranovic.safet@gmail.com>
2022-03-20 18:58:49 +0100

    navbar functionality

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const gridToggle = document.getElementById('sidebar__button')
 const grid = document.getElementById('container')
+const mainNav = document.getElementById('main-nav')
 
 gridToggle.addEventListener('click', () => {
 
 
     if (grid.classList.contains("container--nav-visible")) {
         grid.classList.remove("container--nav-visible")
+        mainNav.classList.add("nav-hidden")
 
 
         console.log('clicked')
     } else {
 
         grid.classList.add("container--nav-visible")
+        mainNav.classList.remove("nav-hidden")
         console.log('clicked')
     }
 })
