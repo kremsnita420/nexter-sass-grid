4c1d83c63d2d0f2462d02562118ea4291a90b269
kremsnita420 <duranovic.safet@gmail.com>
2022-03-24 13:10:49 +0100

    sidebar transition animation works

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const gridToggle = document.getElementById('sidebar__button')
 const grid = document.getElementById('sidebar')
 const mainNav = document.getElementById('main-nav')
+const scrollTopButton = document.getElementById('scroll-top')
 
+
+// ! Open close navbar
 gridToggle.addEventListener('click', () => {
 
 
@@ -18,6 +21,24 @@ gridToggle.addEventListener('click', () => {
         console.log('clicked')
     }
 })
+
+// ! Scroll to top
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollTopButton.style.transform = "scale(1)";
+    } else {
+        scrollTopButton.style.transform = "scale(0)";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 
 
