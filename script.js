d62626a8f8dc14db4033a8bc5880f422decb7f24
kremsnita420 <duranovic.safet@gmail.com>
2022-03-24 11:12:01 +0100

    sidebar transition animation works

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const gridToggle = document.getElementById('sidebar__button')
-const grid = document.getElementById('container')
+const grid = document.getElementById('sidebar')
 const mainNav = document.getElementById('main-nav')
 
 gridToggle.addEventListener('click', () => {
 
 
-    if (grid.classList.contains("container--nav-visible")) {
-        grid.classList.remove("container--nav-visible")
+    if (grid.classList.contains("sidebar--nav-visible")) {
+        grid.classList.remove("sidebar--nav-visible")
         mainNav.classList.add("nav-hidden")
 
 
         console.log('clicked')
     } else {
 
-        grid.classList.add("container--nav-visible")
+        grid.classList.add("sidebar--nav-visible")
         mainNav.classList.remove("nav-hidden")
         console.log('clicked')
     }
